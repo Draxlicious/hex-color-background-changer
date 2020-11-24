60c295795b3c077ae563f0c25bf38dd51e39a8f0
Lukas Mark Jakobsen <43439073+Draxlicious@users.noreply.github.com>
2020-11-24 10:51:51 +0100

    random hex colors from array

--- js/app.js
+++ js/app.js
@@ -1,19 +1,16 @@
-(function() {
-    const button = document.querySelector('#btn')
-    const body = document.querySelector('body')
-    const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-    const value = document.querySelector('#hex-value')
+let btn = document.querySelector(".btn")
+let body = document.querySelector("body")
+let value = document.querySelector("#hex-value")
+let hexArray = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    button.addEventListener('click', changeHex)
+btn.addEventListener('click', HexColor)
 
-    function changeHex(){
-        let hex = '#'
-
-        for (let i = 0; i < 6; i++){
-            const index = Math.floor(Math.random()*hexValues.length)
-            hex += hexValues[index]
-        }
-        value.textContent = hex
-        body.style.backgroundColor = hex
+function HexColor(){
+    let hex = "#";
+    for(let i = 0; i < 6; i++){
+        let index = Math.floor(Math.random() * hexArray.length)
+        hex += hexArray[index]
     }
-} )()
+    value.textContent = hex
+    body.style.backgroundColor = hex
+}
